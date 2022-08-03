bd097ede885cb6de77e05aa352124b4f14936be8
Tim Hosey <thosey@cloudbees.com>
2022-08-03 16:08:14 -0700

    added gitignore for jars, updated Gemfile, stuckJobs working better, added groovy lib

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'userinput'
+gem 'open-uri'
--- stuckJobs.rb
+++ stuckJobs.rb
@@ -1,12 +1,31 @@
 require 'userinput'
+require 'open-uri'
+require 'json'
 
 # Sets up input prompts for URL, username, and pass
-clusterUri = UserInput.new(message: 'CBCI CJOC URL')
+cjocUri = UserInput.new(message: 'CBCI CJOC URL')
 username = UserInput.new(message: 'CBCI Admin Username')
 password = UserInput.new(message: 'CBCI Cluster Password', secret: true)
 
 # Prompts user for input
-clusterUri = clusterUri.ask
+cjocUri = cjocUri.ask
 username = username.ask
 password = password.ask
 
+if (!File.exist?('./jenkins-cli.jar'))
+  download = URI.open("#{cjocUri}/jnlpJars/jenkins-cli.jar")
+  IO.copy_stream(download, './jenkins-cli.jar')
+else
+  puts "CLI Jar exists. Skipping..."
+end
+
+controllerJson = `java -jar jenkins-cli.jar -s #{cjocUri} -webSocket -auth #{username}:#{password} list-masters`
+controllers = JSON.parse(controllerJson)
+
+controllers['data']['masters'].each do |controller|
+  puts "Processing Controller #{controller['fullName']}..."
+  scriptExec = `java -jar jenkins-cli.jar -s #{controller['url']} -webSocket -auth #{username}:#{password} groovy = < ./lib/runningJobs.groovy`
+end
+
+# Delete the jar
+# File.delete('./jenkins-cli.jar') if File.exist?('./jenkins-cli.jar')
