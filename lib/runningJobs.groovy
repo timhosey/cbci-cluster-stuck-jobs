03895fa8e10ed42fff09915885f8bdad75e3b513
Tim Hosey <thosey@cloudbees.com>
2022-08-04 11:10:38 -0700

    tweaked groovy to output on single line

--- lib/runningJobs.groovy
+++ lib/runningJobs.groovy
@@ -1,13 +1,15 @@
+import jenkins.model.Jenkins
 import groovy.json.JsonOutput
 
 def runningBuilds = Jenkins.instance.getView('All').getBuilds().findAll() { it.isInProgress() }
-LinkedHashMap notificationPayload = [:]
+def count = 0
 runningBuilds.each{ value ->
   String jobName = value.getFullDisplayName()
   int durationInSeconds = (System.currentTimeMillis() - value.getStartTimeInMillis())/1000
   if (durationInSeconds > 30) {
-    println("Job: ${jobName}, running for more than half a minute")
-    println("Running time: " + durationInSeconds)
-    notificationPayload.put(jobName, durationInSeconds + " sec")
+    println("Job '${jobName}' running for " + durationInSeconds + " seconds")
+    count++
   }
 }
+
+println(count + " jobs hanging.")
