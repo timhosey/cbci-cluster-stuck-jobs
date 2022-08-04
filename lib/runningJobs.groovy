f40648761b61fe880f6a2f20a7d59e5dd5111a53
Tim Hosey <thosey@cloudbees.com>
2022-08-04 11:34:28 -0700

    changed stuck status to 30 min

--- lib/runningJobs.groovy
+++ lib/runningJobs.groovy
@@ -6,7 +6,7 @@ def count = 0
 runningBuilds.each{ value ->
   String jobName = value.getFullDisplayName()
   int durationInSeconds = (System.currentTimeMillis() - value.getStartTimeInMillis())/1000
-  if (durationInSeconds > 30) {
+  if (durationInSeconds > 1800) {
     println("Job '${jobName}' running for " + durationInSeconds + " seconds")
     count++
   }
